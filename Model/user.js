43583fd471f73a28924c5d3a0e417aa4b753a4a2
Hemant Dixit <hemantdixit00000@gmail.com>
2025-04-30 22:17:39 +0530

    your meaningful commit message

--- Model/user.js
+++ Model/user.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
-const { type } = require('os')
-const { ref } = require('process')
-
-mongoose.connect("mongodb://127.0.0.1:27017/userDatabase")
-
+const mongoose = require('mongoose');
+require('dotenv').config();
+const MONGO_URI="mongodb+srv://hemantdixit00000:Hemu1234%40@blogsite.zrwnljt.mongodb.net/blogDB"
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 const userSchema=mongoose.Schema({
     username:String,
     email:String,
